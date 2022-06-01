19360ba35d0fd1c4efa06e49f935a894f2e91b45
cyxeIO <cyxe@cyxe.io>
2022-05-31 19:58:18 -0700

    Add files via upload

--- components/HeadSection.js
+++ components/HeadSection.js
@@ -95,7 +95,7 @@ const HeadSection = () => {
                    <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                    <div className="sm:text-center lg:text-left">
                    <h1 className="text-4xl tracking-tight font-extrabold text-yellow-500 sm:text-5xl md:text-6xl">
-                       <span className="block lg:py-3 xl:inline"> </span>
+                       <span className="block lg:py-3 xl:inline">Upload Your NFTs </span>
                        <span className="block dark:text-white xl:inline">Discover, Collect and Sell Simpli SAN NFTs</span>
                        </h1>
                        <p className="mt-3 text-base dark:text-white sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0" >
@@ -123,7 +123,7 @@ const HeadSection = () => {
                </div>
                </div>
                <div className="lg:absolute lg:inset-y-3 lg:right-0 lg:w-1/2">
-               <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://ardapps.com/wp-content/uploads/2021/11/head-1.png" alt="headerSection Image" />
+               <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="cyxe.io/diagram.jpg" alt="headerSection Image" />
                </div>
                </div>
 
